321ea3d2f80e5d956b39a78ff8a271f82bc93ed7
Noodle96 <jorgealfredomarino2@gmail.com>
2019-07-05 21:27:36 -0500

    with spring security

---
+++ ClashRoyale1/src/main/java/com/example/demo/controllers/LoginController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LoginController {
+	@GetMapping("/login")
+	public String login(@RequestParam(value="error", required=false) String error,  Model model, Principal principal,
+			RedirectAttributes flash) {
+		//para evitar que haga doble inicio de sesion
+		if(principal !=  null) {
+			flash.addFlashAttribute("info", "Ya ha iniciado sesion anteriormete");
+			return "redirect:/";
+		}
+		if(error != null) {
+			model.addAttribute("error", "Error al login: Nombre o password Incorrect, Try Again!!");
+			
+		}
+		return "login";
+	}
+}
